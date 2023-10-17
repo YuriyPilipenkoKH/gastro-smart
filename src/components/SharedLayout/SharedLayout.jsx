dea10801d0f7c247cdb9baa3d370a51a00c5f38a
Yuriy Pilipenko <yurik2061@gmail.com>
2023-10-17 19:59:59 +0300

    using fetch

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,9 +2,7 @@ import { Suspense, useState } from "react";
 import { Outlet} from 'react-router-dom';
 import { ContactsWrapp, MainHeader, NavigationWrapp} from "./SharedLayout.styled";
 import { logoGastroSmart } from "images/icons";
-
 import {SectionFooter} from "../SectionFooter/SectionFooter"
-
 import { Navigation } from "components/Navigation/Navigation";
 import { AppBar } from "components/AppBar/AppBar";
 import { FlatButton } from "components/Button/Button";
@@ -16,14 +14,13 @@ import MobileMenu from "components/MobileMenu/MobileMenu";
 
 
 
-
 export const SharedLayout = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
 };
-console.log(isOpen);
+
     return (
       <>
         <MainHeader 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,30 @@ import { HomeWrapper } from "./Page.styled";
 import { LineDishes } from "components/LineDishes/LineDishes";
 import { SectionPopularRecentlyAdded } from "components/SectionPopularRecentlyAdded/SectionPopularRecentlyAdded";
 import { SearchRecipes } from "components/SearchRecipes/SearchRecipes";
-import Slider from "components/Slider/Slider";
-import slide1 from '../images/slider/pancake.jpg'
-import slide2 from '../images/slider/meat.jpg'
-import slide3 from '../images/slider/cereal.jpg'
+import { useEffect, useState } from "react";
+
+
 
 export default function Home() {
-  const slides = [
-    {url: slide1, title: 'slide1'},
-    {url: slide2, title: 'slide2'},
-    {url: slide3, title: 'slide3'},
-    // {url: slide4, title: 'slide4'},
-    // {url: slide5, title: 'slide5'},
-]
+
+ const [recipes, setRecipes] = useState([]);
+ useEffect(() => {
+  // Load the JSON data from the public directory
+  fetch('../json/recipes.json') // This assumes the JSON file is in the public directory
+    .then((response) => {
+      console.log(response)
+      response.json()
+    })
+    .then((data) => setRecipes(data))
+    .catch((error) => console.error('Error loading JSON:', error));
+}, []);
+
+console.log('recipes',recipes)
 
     return (
       <HomeWrapper  >
        <SearchRecipes/>
-         <Slider slides={slides}/>
+         {/* <Slider slides={slides}/> */}
          <LineDishes/>
          <SectionPopularRecentlyAdded/>
       
