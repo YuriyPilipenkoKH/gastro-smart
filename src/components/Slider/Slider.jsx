8c51e3fdd3ccf2dd3e82761b2df47678faa9188b
Yuriy Pilipenko <yurik2061@gmail.com>
2023-10-18 12:04:44 +0300

    fix getPreviousSubarray()

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -18,20 +18,24 @@ const Slider = ({ slides }) => {
   }
   // Function to get the previous subarray and update the index
 function getPreviousSubarray() {
+
   const subarray = [];
-  for (let i = 2; i >= 0; i--) {
-    subarray.push(array[(currentIndex + i) % array.length]);
+
+  for (let i = 0; i < 3; i++) {
+    const index = (currentIndex - i + array.length) % array.length;
+    subarray.unshift(array[index]);
   }
-  setCurrentIndex( (currentIndex - 1 + array.length) % array.length);
+
+  setCurrentIndex ((currentIndex - 1 + array.length) % array.length);
   return subarray;
 }
   // Function to get the next subarray and update the index
 function getNextSubarray() {
   const subarray = [];
-  for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
     subarray.push(array[(currentIndex + i) % array.length]);
   }
-  setCurrentIndex ((currentIndex + 1) % array.length);
+  setCurrentIndex  ((currentIndex + 1) % array.length);
   return subarray;
 }
 
