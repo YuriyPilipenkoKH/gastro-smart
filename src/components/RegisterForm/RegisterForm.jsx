30bdfb192e94c2c6be1972b89a7fc018f8af41a2
Yuriy Pilipenko <yurik2061@gmail.com>
2023-10-25 09:03:41 +0300

    emailAvailable function implemented

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -100,6 +100,11 @@ const RegisterForm = () => {
                                 'This domain is not supported'
                             )
                         },
+                        emailAvailable: async (fieldValue)  => {
+                            const response = await fetch(`https://jsonplaceholder.typicode.com/users?email=${fieldValue}`)
+                            const data = await response.json()
+                            return data.length === 0 || 'Email already exists'
+                        },
                     }
                 })}
 
